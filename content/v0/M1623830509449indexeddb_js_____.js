c57b9d0c26e38d20631bfb4aff907429acad8adf
Maygician <maygicinc@gmail.com>
2021-06-16 04:39:36 -0400

    idb wip

--- content/v0/M1623830509449indexeddb_js_____.js
+++ content/v0/M1623830509449indexeddb_js_____.js
@@ -6,10 +6,17 @@ async function M1623821216754indexeddb_opendb_(M16238212857720J4238014379729269)
     window[M16238212857720J4238014379729269[0]] = M1623820653095indexedb_________.open(M16238212857720J4238014379729269[1],M16238212857720J4238014379729269[2])
 }
 var M1623830819132first_idb_connect;
+var M1623832698492idb_sample_store_;
 var M1623831893386db_______________;
 async function M1623830709556indexeddb_init___(){
    await M1623821216754indexeddb_opendb_(["M1623830819132first_idb_connect","M16238307510080J7194791896383828",1]);
-//    M1623830819132first_idb_connect.onupgradeneeded =
+   M1623830819132first_idb_connect.onupgradeneeded = function(event) {
+    // Save the IDBDatabase interface
+    var M1623831893386db_______________ = event.target.result;
+  
+    // Create an objectStore for this database
+    M1623832698492idb_sample_store_ = M1623831893386db_______________.createObjectStore("name", { keyPath: "myKey" });
+  };
 //    M1623830819132first_idb_connect.onerror = function(e){console.log(e)}
    M1623830819132first_idb_connect.onsuccess = function() {
     M1623831893386db_______________ = M1623830819132first_idb_connect.result;
@@ -19,5 +26,4 @@ async function M1623830709556indexeddb_init___(){
       alert("Database is outdated, please reload the page.")
     };
 }
-M1623831893386db_______________.createObjectStore("M1623831451834idb_descript_stor")
 }
