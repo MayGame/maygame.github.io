1ae0c5d66a3ef7619a477767c552a036141c1a98
Maygician <maygicinc@gmail.com>
2021-06-16 05:36:58 -0400

    idb wip

--- content/v0/M1623830509449indexeddb_js_____.js
+++ content/v0/M1623830509449indexeddb_js_____.js
@@ -11,6 +11,7 @@ var M1623831893386db_______________;
 async function M1623830709556indexeddb_init___(){
    await M1623821216754indexeddb_opendb_(["M1623830819132first_idb_connect","M16238307510080J7194791896383828",1]);
    M1623830819132first_idb_connect.onupgradeneeded = function(event) {
+    //      if (event.oldVersion < 1) {
     // Save the IDBDatabase interface
     M1623831893386db_______________ = event.target.result;
   
@@ -28,4 +29,22 @@ async function M1623830709556indexeddb_init___(){
       alert("Database is outdated, please reload the page.")
     };
 }
+}
+function M1623834175347idb_add_descripti(M16238341077200J4965506095461713){
+    let key=M16238341077200J4965506095461713.id;
+    let value = M16238341077200J4965506095461713.desc;
+    M1623831893386db_______________.transaction("M1623831451834idb_descript_stor","readwrite").objectStore("M1623831451834idb_descript_stor").add(value, key)
+}
+function M1623834654341get_desc_for_____(M16238346618430J0161460206740422)
+{
+   M1623831893386db_______________.transaction("M1623831451834idb_descript_stor","readonly")
+   .objectStore("M1623831451834idb_descript_stor").get(M16238346618430J0161460206740422.id).onsuccess=function(event){
+    window[M16238346618430J0161460206740422.where_put]=event.target.result;
+   }
+}
+var M1623836072764get_desc_result__;
+async function M1623836116952get_desc_example_(){
+   await M1623834175347idb_add_descripti({"id":"M1623834654341get_desc_for_____","desc":"Retrieves description for 'id', puts it into 'where_put'"})
+    M1623834654341get_desc_for_____({"id":"M1623834654341get_desc_for_____","where_put":"M1623836072764get_desc_result__"})
+    
 }
