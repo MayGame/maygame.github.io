3962a4985717f8fd5ccae990958d8ea76e7c3cd9
Maygician <maygicinc@gmail.com>
2021-07-01 00:48:23 -0400

    pre-corr wip

--- content/v0/M1625062653871JesSiCore________.js
+++ content/v0/M1625062653871JesSiCore________.js
@@ -1,11 +1,11 @@
 var M1622519980405clairdlwlrMayJesCA = true;
 
-var M1625068079944JesSWindowProxXxy;
+var M1625068079944JesSWindowProxXxy=window;
 var M1625063417462CoreToLoad_______;
 function M1625063654235InitVarrssksks___(){
-    M1625068079944JesSWindowProxXxy=window;
+    // M1625068079944JesSWindowProxXxy=window;
     // M1625063417462CoreToLoad_______=
-    M1625068079944JesSWindowProxXxy[M1625063417462CoreToLoad_______]
+    // M1625068079944JesSWindowProxXxy[M1625063417462CoreToLoad_______]
     //()=>console.log("V-1");
 
 }
