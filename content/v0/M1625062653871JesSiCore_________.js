1a6cfb97ff84256285674c8257d085e0996ebd7e
Maygician <maygicinc@gmail.com>
2021-07-04 10:21:53 -0400

    Ser +key

--- content/v0/M1625062653871JesSiCore_________.js
+++ content/v0/M1625062653871JesSiCore_________.js
@@ -77,11 +77,11 @@ var M1623774638130idb_transaction___ = M1625068079944JesSWindowProxXxy_.IDBTrans
 var M1623820653095indexedb__________ = M1625068079944JesSWindowProxXxy_.indexedDB || M1625068079944JesSWindowProxXxy_.mozIndexedDB || M1625068079944JesSWindowProxXxy_.webkitIndexedDB || M1625068079944JesSWindowProxXxy_.msIndexedDB;
 var M1623820745971idb_keyrange______ = M1625068079944JesSWindowProxXxy_.IDBKeyRange || M1625068079944JesSWindowProxXxy_.webkitIDBKeyRange || M1625068079944JesSWindowProxXxy_.msIDBKeyRange;
 
-async function M1623821216754indexeddb_opendb__(M16238212857720J4238014379729269){
+var M1623821216754indexeddb_opendb__=async function (M16238212857720J4238014379729269){
     M1625068079944JesSWindowProxXxy_[M16238212857720J4238014379729269[0]] = M1623820653095indexedb__________
     .open(M16238212857720J4238014379729269[1],M16238212857720J4238014379729269[2])
 }
-async function M1623941545551idb_Mayn_db_init__(){
+var M1623941545551idb_Mayn_db_init__=async function (){
     await M1623821216754indexeddb_opendb__(["M1623943250216idb_Mayn_db_cnct__",M1623942055880idb_Mayn_db_name__,2]);
     M1623943250216idb_Mayn_db_cnct__.onupgradeneeded = function(event) {
         M1623941499181idb_Mayn_db_______ = event.target.result;
@@ -97,7 +97,7 @@ async function M1623941545551idb_Mayn_db_init__(){
             M1623941499181idb_Mayn_db_______.close();
           alert("Database is outdated, please reload the page.")
 }}}
-function M1625300496229IDBThesaurusGetCB_(M16253001282300J6024000346344986){
+var M1625300496229IDBThesaurusGetCB_ = function(M16253001282300J6024000346344986){
     let key = M16253001282300J6024000346344986.key;
     let callback = M16253001282300J6024000346344986.cb;
     let store = M1623941499181idb_Mayn_db_______.transaction('M1623945792684idb_thesaurus_____',"readonly").objectStore("M1623945792684idb_thesaurus_____")
@@ -106,7 +106,7 @@ function M1625300496229IDBThesaurusGetCB_(M16253001282300J6024000346344986){
 }
 //#endregion
 //#region core
-function M1625294924126SubCorr___________(){
+var M1625294924126SubCorr___________ = function(){
     console.log("Hi, Mistress")
 }
 var M1625297347110Deserial_function_=function(M16252973756790J2619777814455608){
@@ -135,26 +135,38 @@ var M1625320414108Func_Get_Arg_Name_=function(M16253205254550J1305364840200156){
     // else return "";
     return result||"";
 }
-function M1625306744551funcStrGetRelated_(M16253067937360J4115575867813261){
+var M1625306744551funcStrGetRelated_ = function(M16253067937360J4115575867813261){
     const result = [...M16253067937360J4115575867813261.matchAll(M1625310367858ID_RegExp_________)];
     let flet = [];
     result.forEach(e=>{flet.push(e[0])})
     return [...new Set(flet)];
 }
-var M1625301090123Srialize_function_ = function(M16253011146210J9441376555473651){//arg - string(id)
-    let key = M16253011146210J9441376555473651;
+//M1625406744808Type_JS_Function__
+var M1625301090123Srialize_function_ = function(M16253011146210J9441376555473651){//arg - string(id) or obj
+    let key;
+    let text;
     let transaction=M1623941499181idb_Mayn_db_______.transaction(['M1623945792684idb_thesaurus_____',
-    'M1623940897095idb_func_arg_stor_','M1623945927273idb_related_vars__','M1625105007943IDBStoreMeta______'],"readwrite")
+    'M1623940897095idb_func_arg_stor_','M1623945927273idb_related_vars__','M1625105007943IDBStoreMeta______','M1623940635385idb_type_map______'],"readwrite")
     let thes = transaction.objectStore("M1623945792684idb_thesaurus_____");
-    let text = window[key].toString();
+    if(typeof M16253011146210J9441376555473651=="object"){
+        key=M16253011146210J9441376555473651.key;
+        text=M16253011146210J9441376555473651.text;
+    if(typeof M16253011146210J9441376555473651=="string"){
+        key = M16253011146210J9441376555473651;
+        text = window[key].toString();
+}
     let fnbody = M1625368437043SerSimpl__________(text)
     let arg = M1625320414108Func_Get_Arg_Name_(text);
     thes.put({text:text,body:fnbody,arg:arg,key:key},key);
     let meta={source:"",modified:Date.now()};
+    
+    transaction.objectStore("'M1623940635385idb_type_map______'").put('M1625406744808Type_JS_Function__',key);
     transaction.objectStore("M1625105007943IDBStoreMeta______").put(meta,key);
     transaction.objectStore("M1623945927273idb_related_vars__").put(M1625306744551funcStrGetRelated_(text),key);
     transaction.objectStore("M1623940897095idb_func_arg_stor_").put(key,arg);
 }
+}
+
 var M1625368437043SerSimpl__________ = function(M16253684599980J0384719987037425){
 //    let text = M1625068079944JesSWindowProxXxy_[M16253684599980J0384719987037425].toString();
    let text = M16253684599980J0384719987037425.toString();
@@ -168,7 +180,7 @@ var M1624265066524rand_id___________=function(){
     //starts with a number, contains dot -> not a valid ID for C-like languages
     //31-35 symbols. 
     return id;}
-function M1624264785245value_to_id_______(M16242647363800J5140259437325456){
+var M1624264785245value_to_id_______ = function(M16242647363800J5140259437325456){
 let a=('M'+Date.now()+M16242647363800J5140259437325456.value+'_________________________').slice(0,32);
 return a.replaceAll(' ','_');
 }
