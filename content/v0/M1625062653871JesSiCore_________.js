c258abce53b494360ebad78ce2438e13fdfb213f
Maygician <maygicinc@gmail.com>
2021-07-04 06:41:26 -0400

    get arg fix

--- content/v0/M1625062653871JesSiCore_________.js
+++ content/v0/M1625062653871JesSiCore_________.js
@@ -127,7 +127,9 @@ var M1625297347110Deserial_function_=function(M16252973756790J2619777814455608){
 // let fa = new Function()
 var M1625310367858ID_RegExp_________ =new RegExp(/M\d{13}\w{18}/,'g')
 var M1625320414108Func_Get_Arg_Name_=function(M16253205254550J1305364840200156){
-    let result = M16253205254550J1305364840200156.match(/function*?\(\W*(\w*)/);
+    // let result = M16253205254550J1305364840200156.match(/function*?\(\W*(\w*)/);
+    let fnbody = text.substring(text.indexOf("(")+1,text.indexOf(")"))
+
     if(result)
     return result[1];
     else return "";
