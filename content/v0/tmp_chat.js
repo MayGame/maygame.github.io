544a22ddec8b1a26b6b94a94a3d6483f88948ac0
Maygician <maygicinc@gmail.com>
2021-07-13 09:55:11 +0900

    new word finalized

--- content/v0/tmp_chat.js
+++ content/v0/tmp_chat.js
@@ -1,10 +1,10 @@
 var common_message_queue=[];
 // M1623941545551idb_Mayn_db_init__();
 // var gun = new Gun({localStorage:false, peers:["https://gun-manhattan.herokuapp.com/gun"]});
-var gun = new Gun({localStorage:false, peers:["https://mg-gun.herokuapp.com/gun"]});
-// var gun = new Gun({localStorage:false, peers:["https://vine-mature-morocco.glitch.me/gun"]});
-var sea = Gun.SEA
-var user = gun.user()
+var M1626125425225mg_gunHerokuGunIns = new Gun({localStorage:false, peers:["https://mg-gun.herokuapp.com/gun"]});
+// var M1626131872934vmGlitchGunInstanc = new Gun({localStorage:false, peers:["https://vine-mature-morocco.glitch.me/gun"]});
+var sea = Gun.SEA;
+var user = M1626125425225mg_gunHerokuGunIns.user()
 var me;
 var chatWindow=`
 <label for="choose_me">Me:</label>
@@ -66,12 +66,12 @@ update_logs()
 // M1624262716713UISpawnNewWindow__({content:"m", title:"p1"})
 }
 var word_for_type_message="M1625913370169MessageDBonher____";
-var word_for_introduce_new_word="M1625908424615NewMJWordToInspire";
-gun.get("tmp").get("cmq").on(oncmq)
-// gun.get("tmp").get("cmq").on(e=>oncmq(e))
+var word_for_introduce_new_word="M1626131452199MJWordOffering____";
+M1626125425225mg_gunHerokuGunIns.get("M1626125031687MGGunUnrPubPutChTp").get("cmq").on(oncmq)
+// M1626125425225mg_gunHerokuGunIns.get("M1626125031687MGGunUnrPubPutChTp").get("cmq").on(e=>oncmq(e))
 function test_send(){
 let id = Date.now()
-    gun.get("tmp").get("cmq").put(JSON.stringify({from:1,receiver:1,content:"hi",id:id}))
+    M1626125425225mg_gunHerokuGunIns.get("M1626125031687MGGunUnrPubPutChTp").get("cmq").put(JSON.stringify({from:1,receiver:1,content:"hi",id:id}))
 }
 function nts_id(){
 let Jrand = (Math.random()+"000000").replace('.','J').substr(0,18);
@@ -80,22 +80,25 @@ return res;
 }
 function send(content, from, to){
 let id = Date.now()
-    gun.get("tmp").get("cmq").put(JSON.stringify({from:from,receiver:to,content:content,id:id}))
+    M1626125425225mg_gunHerokuGunIns.get("M1626125031687MGGunUnrPubPutChTp").get("cmq").put(JSON.stringify({from:from,receiver:to,content:content,id:id}))
 }
 function sendv2(content, from, to){
-//gun.get("tmp").get(
+//M1626125425225mg_gunHerokuGunIns.get("M1626125031687MGGunUnrPubPutChTp").get(
 }
 function update_logs(){
     let el = common_message_queue.pop();
     let txt=`<br><p onclick="M1624635774325Toast_____________({text:'at ${new Date(el.id)}'})">${el.from}: ${el.content}</p> `
     document.getElementById('chat_log').insertAdjacentHTML("afterbegin",txt)   
 }
-
+var M1626125031687MGGunUnrPubPutChTp;
 // function save_to_idb(msg){
 //     let store = M1623941499181idb_Mayn_db_______.transaction(
 //         ["M1623945792684idb_thesaurus_____","M1623940635385idb_type_map______",
 //     "M1625105007943IDBStoreMeta______"],"readwrite").objectStore("M1623945792684idb_thesaurus_____");
 // }
+var tmpname4M1626125031687MGGunUnrPubPutChTpHandler = function(newCh2listen){
+M1626125425225mg_gunHerokuGunIns.get("M1626125031687MGGunUnrPubPutChTp").get("newCh2listen").on(fixme)
+}
 const userMediaConstraints = {
     audio: true,
     video: false
